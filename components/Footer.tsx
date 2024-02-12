9a7c9d3571032562d24ea1fad93303022a41dab9
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-02-12 09:16:14 +0500

    fixed links and changed the text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,27 +3,29 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="flex items-center gap-5 py-8 mt-8 max-sm:justify-center max-sm:flex-wrap">
-      <p className="text-xl max-sm:text-lg">© 2024</p>
-      <Link
-        href={"https://www.twitter.com"}
-        target="_blank"
-        className="text-xl max-sm:text-lg"
-      >
-        Twitter
-      </Link>
-      <Link
-        href={"https://www.linkedin.com"}
-        className="text-xl max-sm:text-lg"
-      >
-        LinkedIn
-      </Link>
-      <Link
-        href={"mailto:KkV0D@example.com"}
-        className="text-xl max-sm:text-lg"
-      >
-        Email
-      </Link>
+    <div className="flex items-center gap-5 py-8 mt-8 justify-between max-sm:flex-wrap">
+      <p className="text-xl max-sm:text-lg">Made with ❤️.</p>
+      <div className="flex items-center gap-5 self-end">
+        <Link
+          href={"https://www.twitter.com"}
+          target="_blank"
+          className="text-xl max-sm:text-lg"
+        >
+          Twitter
+        </Link>
+        <Link
+          href={"https://www.linkedin.com"}
+          className="text-xl max-sm:text-lg"
+        >
+          LinkedIn
+        </Link>
+        <Link
+          href={"mailto:KkV0D@example.com"}
+          className="text-xl max-sm:text-lg"
+        >
+          Email
+        </Link>
+      </div>
     </div>
   );
 }
