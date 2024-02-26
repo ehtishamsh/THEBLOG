4b298920980462f4ab9273edce48e092c8c668aa
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-02-26 06:47:54 +0500

    added more public paths

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ import { authOptions } from "./lib/auth";
 import { useSession } from "next-auth/react";
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const isPublicPath = path == "/sign-in" || path == "/sign-up";
+  const isPublicPath =
+    path == "/sign-in" || path == "/sign-up" || path == "/" || path == "/home";
   const tokken = request.cookies?.get("next-auth.session-token")?.value || "";
   if (isPublicPath && tokken) {
     return NextResponse.redirect(new URL("/home", request.url));
