6c71e7ff95c6bd80d0005d99e07efca8f4e09a5f
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-03-01 08:50:29 +0500

    fixed data fetch

--- components/blogpage/DetailBlog.tsx
+++ components/blogpage/DetailBlog.tsx
@@ -1,7 +1,7 @@
-"use client";
 import React, { useEffect, useState } from "react";
 import GetColor from "../utils/GetColor";
 import GetContent from "./GetContent";
+import Loading from "@/app/(dashboard)/admin/loading";
 
 interface blog {
   title: string;
@@ -9,45 +9,41 @@ interface blog {
   image: string;
   tags: string[];
 }
-function DetailBlog({ params }: { params: string }) {
-  const [blog, setBlog] = useState<blog>({} as blog);
-  useEffect(() => {
-    const fetchData = async () => {
-      const slug = params;
-      const data = await fetch(`http://localhost:3000/api/user/blogs/${slug}`, {
-        method: "GET",
-      });
-      const blog = await data.json();
-      setBlog(blog?.formatedBlog);
-    };
-    fetchData();
-    return () => {
-      setBlog({} as blog);
-    };
-  }, []);
+async function DetailBlog({ params }: { params: string }) {
+  const slug = params;
+  const fetchdata = await fetch(
+    `http://localhost:3000/api/user/blogs/${slug}`,
+    {
+      method: "GET",
+    }
+  );
+  const data = await fetchdata.json();
+  const blog = data.formatedBlog;
 
   return (
-    <div className="transition-all duration-400 px-2 flex flex-col justify-start items-start  mt-3">
-      <p className="transition-all duration-400 text-base font-semibold mb-8 dark:text-purple-900 text-purple-700 ">
-        Sunday , 1 Jan 2023
-      </p>
-      <h1 className="transition-all duration-400 text-4xl font-bold mb-8">
-        {blog?.title}
-      </h1>
-      <img
-        src={blog?.image}
-        alt={blog?.title}
-        className="object-cover w-full max-h-[500px] mb-8"
-      />
-      <div className="transition-all duration-400 gap-8 flex flex-col justify-start items-start">
-        <GetContent content={blog?.content} />
+    <>
+      <div className="transition-all duration-400 px-2 flex flex-col justify-start items-start  mt-3">
+        <p className="transition-all duration-400 text-base font-semibold mb-8 dark:text-purple-900 text-purple-700 ">
+          Sunday , 1 Jan 2023
+        </p>
+        <h1 className="transition-all duration-400 text-4xl font-bold mb-8">
+          {blog?.title}
+        </h1>
+        <img
+          src={blog?.image}
+          alt={blog?.title}
+          className="object-cover w-full max-h-[500px] mb-8"
+        />
+        <div className="transition-all duration-400 gap-8 flex flex-col justify-start items-start">
+          <GetContent content={blog?.content} />
+        </div>
+        <div className="mt-8 flex gap-3">
+          {blog?.tags?.map((tag: string, index: number) => (
+            <GetColor tagName={tag} key={index} />
+          ))}
+        </div>
       </div>
-      <div className="mt-8 flex gap-3">
-        {blog?.tags?.map((tag: string, index: number) => (
-          <GetColor tagName={tag} key={index} />
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
 
