1b659fa9a998d48bc96dd9fd93b43cc9e5b2c8af
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-03-01 06:36:51 +0500

    some change

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,15 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "./lib/auth";
 import { useSession } from "next-auth/react";
-export function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
-  const isPublicPath = path == "/sign-in" || path == "/sign-up";
-  const tokken = request.cookies?.get("next-auth.session-token")?.value || "";
-  if (isPublicPath && tokken) {
-    return NextResponse.redirect(new URL("/home", request.url));
-  }
-  return NextResponse.next();
-}
+// export function middleware(request: NextRequest) {
+//   const path = request.nextUrl.pathname;
+//   const isPublicPath = path == "/sign-in" || path == "/sign-up";
+//   const tokken = request.cookies?.get("next-auth.session-token")?.value || "";
+//   if (isPublicPath && tokken) {
+//     return NextResponse.redirect(new URL("/home", request.url));
+//   }
+//   return NextResponse.next();
+// }
 export const config = {
   matcher: ["/admin/:path*", "/sign-in", "/sign-up", "/home", "/"],
 };
