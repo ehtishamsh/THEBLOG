bdaee965d1cd67ea365471eddd203d02d174f555
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-04-09 05:36:44 +0500

    added animation

--- components/userPage/NavbarDashBoard.tsx
+++ components/userPage/NavbarDashBoard.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { usePathname } from "next/navigation";
-import React, { MouseEventHandler, useState } from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { X } from "lucide-react";
+import { animate, motion } from "framer-motion";
 
 function NavbarDashBoard() {
   const [open, setOpen] = useState<boolean>(false);
@@ -14,23 +15,43 @@ function NavbarDashBoard() {
       <div className="hidden max-sm:block">
         <Button
           variant="outline"
-          className={`p-1 ${open ? "z-[9999] absolute top-4 left-4" : ""}`}
+          className={`p-1 ${open ? "z-[9999] absolute top-8 left-5" : ""}`}
           onClick={() => setOpen((prev) => !prev)}
         >
-          {open ? (
-            <X width={25} height={25} />
-          ) : (
-            <HamburgerMenuIcon width={25} height={25} />
-          )}
+          <motion.div>
+            {open ? (
+              <motion.span>
+                <X width={25} height={25} />
+              </motion.span>
+            ) : (
+              <HamburgerMenuIcon width={25} height={25} />
+            )}
+          </motion.div>
         </Button>
-        {path === "/user/blogs" || path === "/user/profile" ? (
-          <div
+        {open ? (
+          <motion.div
+            variants={{
+              hidden: { x: "100%", opacity: 0, transition: { duration: 0.2 } },
+              visible: {
+                x: "0%",
+                opacity: 1,
+                transition: { staggerChildren: 0.3 },
+              },
+            }}
+            initial="hidden"
+            animate={open ? "visible" : "hidden"}
+            transition={{
+              type: "spring",
+              stiffness: 260,
+              damping: 20,
+              duration: 1,
+            }}
             className={`${
-              open ? "block" : "hidden"
-            } text-white w-[65vw] h-screen bg-black absolute top-0 left-0 z-50`}
+              open ? "visible" : "invisible"
+            } text-white w-[65vw] h-screen bg-background border border-border absolute top-0 left-0 z-50`}
           >
-            <div className="w-full h-full relative"></div>
-          </div>
+            <div className="w-full h-full relative">fdfddfdf</div>
+          </motion.div>
         ) : null}
       </div>
     </>
