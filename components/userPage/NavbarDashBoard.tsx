94569afc98952d80797a2aa086f0071aee8ebc03
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-04-10 05:36:07 +0500

    added links

--- components/userPage/NavbarDashBoard.tsx
+++ components/userPage/NavbarDashBoard.tsx
@@ -3,8 +3,10 @@ import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 import { Button } from "../ui/button";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
-import { X } from "lucide-react";
+import { Home, X } from "lucide-react";
 import { animate, motion } from "framer-motion";
+import Link from "next/link";
+import { BiSolidBusiness } from "react-icons/bi";
 
 function NavbarDashBoard() {
   const [open, setOpen] = useState<boolean>(false);
@@ -31,7 +33,7 @@ function NavbarDashBoard() {
         {open ? (
           <motion.div
             variants={{
-              hidden: { x: "100%", opacity: 0, transition: { duration: 0.2 } },
+              hidden: { x: "-100%", opacity: 0, transition: { duration: 0.2 } },
               visible: {
                 x: "0%",
                 opacity: 1,
@@ -48,9 +50,54 @@ function NavbarDashBoard() {
             }}
             className={`${
               open ? "visible" : "invisible"
-            } text-white w-[65vw] h-screen bg-background border border-border absolute top-0 left-0 z-50`}
+            } text-white w-[50vw] h-screen bg-background border border-border absolute top-0 left-0 z-50`}
           >
-            <div className="w-full h-full relative">fdfddfdf</div>
+            <div className="w-full h-full relative mt-24 flex flex-col gap-4">
+              <Link
+                href={"/home"}
+                className={`text-lg text-center min-w-max block mx-3 px-2 py-2 max-xs:text-base hover:bg-accent ${
+                  path === "/home" ? "bg-accent" : ""
+                } transition-all duration-300 rounded-md border border-border flex justify-center items-center gap-3`}
+              >
+                <span>
+                  <Home width={25} height={25} />
+                </span>
+                Home
+              </Link>
+              <Link
+                href={"https://ehtishamshah.vercel.app/"}
+                target="_blank"
+                className={`text-lg text-center min-w-max  mx-3 px-2 py-2 max-xs:text-base hover:bg-accent transition-all duration-300 rounded-md border border-border flex justify-center items-center gap-3`}
+              >
+                <span>
+                  <BiSolidBusiness width={25} height={25} />
+                </span>
+                Portfolio
+              </Link>
+              {path === "/user/profile" || path === "/user/blogs" ? (
+                <div className="flex flex-col gap-4">
+                  <h1 className="text-SM  text-center text-accent">
+                    User Dashboard
+                  </h1>
+                  <Link
+                    href={"/user/profile"}
+                    className={`text-lg text-center min-w-max block mx-3 px-2 py-2 max-xs:text-base hover:bg-accent ${
+                      path === "/user/profile" ? "bg-accent" : ""
+                    } transition-all duration-300 rounded-md border border-border flex justify-center items-center gap-3`}
+                  >
+                    Profile
+                  </Link>
+                  <Link
+                    href={"/user/blogs"}
+                    className={`text-lg text-center min-w-max block mx-3 px-2 py-2 max-xs:text-base hover:bg-accent ${
+                      path === "/user/blogs" ? "bg-accent" : ""
+                    } transition-all duration-300 rounded-md border border-border`}
+                  >
+                    Your Blogs
+                  </Link>
+                </div>
+              ) : null}
+            </div>
           </motion.div>
         ) : null}
       </div>
