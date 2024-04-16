81debb877a261f68e678a426f116c52f994e8880
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-04-17 04:09:45 +0500

    moved up usePathname

--- app/(user)/(dashboard)/user/page.tsx
+++ app/(user)/(dashboard)/user/page.tsx
@@ -4,10 +4,10 @@ import { redirect, usePathname } from "next/navigation";
 
 function Page() {
   const { data: session } = useSession();
+  const pathname = usePathname();
   if (session === null) {
     return redirect("/sign-in");
   }
-  const pathname = usePathname();
   if (pathname === "/user") {
     return redirect("/user/profile");
   }
