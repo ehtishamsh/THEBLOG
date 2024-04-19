f78b79a884bca69d5d9ee12c34ca89e464d97284
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-04-20 04:39:11 +0500

    removed use client

--- components/homepage/AllBlogData.tsx
+++ components/homepage/AllBlogData.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
 import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 import GetColor from "../utils/GetColor";
