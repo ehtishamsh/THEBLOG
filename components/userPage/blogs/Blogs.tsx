b967e2fd2a9ed9a9d3a249c6d9f765174424e5c4
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-04-22 03:28:04 +0500

    added loading to user blogs

--- components/userPage/blogs/Blogs.tsx
+++ components/userPage/blogs/Blogs.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { columns } from "@/app/(user)/(dashboard)/user/blogs/columns";
 import { DataTable } from "@/app/(user)/(dashboard)/user/blogs/data-table";
+import { Loader2 } from "lucide-react";
 import React, { useEffect, useState } from "react";
 
 export default function Blogs() {
   const [data, setData] = useState<any>();
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     const fetchBlogs = async () => {
       try {
         const fetchData = await fetch("/api/user/profile", {
@@ -13,6 +16,7 @@ export default function Blogs() {
         });
         const getData = await fetchData.json();
         setData(getData);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -25,7 +29,13 @@ export default function Blogs() {
 
   return (
     <div className=" mx-auto pb-8 max-w-7xl">
-      <DataTable columns={columns} data={data?.blogs || []} />
+      {loading ? (
+        <div className="flex justify-center items-center h-[40vh] max-md:h-[30vh] animate-pulse">
+          <Loader2 className="animate-spin h-14 w-14 max-sm:w-full " />
+        </div>
+      ) : (
+        <DataTable columns={columns} data={data?.blogs || []} />
+      )}
     </div>
   );
 }
