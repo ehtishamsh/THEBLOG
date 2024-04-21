58eccb6fcdfb6230eb94b2b333462559b8c51c96
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-04-22 04:47:31 +0500

    added links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,26 +5,20 @@ function Footer() {
   return (
     <div className="px-2">
       <div className="max-w-7xl mx-auto flex items-center gap-5 py-8 mt-8 justify-between max-sm:flex-wrap">
-        <p className="text-xl max-sm:text-lg">Made with ❤️.</p>
+        <p className="text-xl max-sm:text-lg">Made with ❤️ by Ehtisham.</p>
         <div className="flex items-center gap-5 self-end">
           <Link
-            href={"https://www.twitter.com"}
+            href={"https://github.com/ehtishamsh"}
             target="_blank"
             className="text-xl max-sm:text-lg"
           >
-            Twitter
+            Github
           </Link>
           <Link
-            href={"https://www.linkedin.com"}
+            href={"https://www.linkedin.com/in/ehtishamshah/"}
             className="text-xl max-sm:text-lg"
           >
             LinkedIn
-          </Link>
-          <Link
-            href={"mailto:KkV0D@example.com"}
-            className="text-xl max-sm:text-lg"
-          >
-            Email
           </Link>
         </div>
       </div>
