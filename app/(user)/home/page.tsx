ded8abc6b027425599f38e3af18bf6a8c0d7569f
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-04-21 06:48:05 +0500

    removed pagination

--- app/(user)/home/page.tsx
+++ app/(user)/home/page.tsx
@@ -4,14 +4,13 @@ import Pagination from "@/components/homepage/Pagination";
 import RecentBlogs from "@/components/homepage/RecentBlogs";
 import React from "react";
 
-function page() {
+async function page({ searchParams }: { searchParams: { page: string } }) {
   return (
     <>
       <HeroSection />
       <div className="max-w-7xl mx-auto mt-8 px-5">
         <RecentBlogs />
-        <AllBlogSection />
-        <Pagination />
+        <AllBlogSection page={Number(searchParams.page)} />
       </div>
     </>
   );
