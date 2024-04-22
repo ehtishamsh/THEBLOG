c69fbd8e4668366c11ae3ad16fed3dcc1c9ebedc
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-04-22 05:08:31 +0500

    added more margin

--- components/homepage/HeroSection.tsx
+++ components/homepage/HeroSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function HeroSection() {
   return (
-    <div className="max-w-7xl mx-auto mt-44">
+    <div className="max-w-7xl mx-auto mt-40">
       <h1 className="p-5 text-center text-[243px] max-xl:text-[200px] max-lg:text-[150px] max-md:text-[100px] max-sm:text-[80px] max-xs:text-[60px] transition-all duration-300 font-bold border-t-2 dark:border-gray-700 border-gray-300 border-b-2 ">
         THE BLOG
       </h1>
